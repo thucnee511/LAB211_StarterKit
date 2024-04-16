f247c3e1af3f20d539d68bb067266eb46e77359c
ngocngocthuc@gmail.com <ngocngocthuc@gmail.com>
2024-04-16 17:36:13 +0100

    refactor: Repository interface:

--- src/vn/edu/fpt/lab/repositories/IRepository.java
+++ src/vn/edu/fpt/lab/repositories/IRepository.java
@@ -1,5 +1,16 @@
 package vn.edu.fpt.lab.repositories;
 
+import java.util.List;
+
 public interface IRepository<T> {
-    
+
+    List<T> get() throws Exception;
+
+    T get(String id) throws Exception;
+
+    T post(T item) throws Exception;
+
+    T put(T item) throws Exception;
+
+    T delete(T item) throws Exception;
 }
